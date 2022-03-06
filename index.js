d7a89aa8f66c17eab56cd7f88d77f0a451a07c7b
Mukund Harikumar <74809793+zeus1347@users.noreply.github.com>
2022-03-07 01:23:27 +0400

    First commit

---
+++ index.js
@@ -0,0 +1,16 @@
+let countEl=document.getElementById("count-el")
+let saveEl=document.getElementById("save-el")
+
+let count=0
+
+function increment(){
+    count+=1
+    countEl.textContent=count
+}
+
+function save(){
+    let saveStr=count + "-"
+    saveEl.textContent+= saveStr
+    count=0
+    countEl.textContent=0
+}
